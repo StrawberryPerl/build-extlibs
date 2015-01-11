28b1d7eb05bb0f7960c39d26085ff5ccdf0c2717
kmx <kmx@cpan.org>
2015-01-11 22:29:49 +0100

    reverting bat

--- msys2_shell_483_32.bat
+++ msys2_shell_483_32.bat
@@ -2,4 +2,4 @@
 set GCCDIR=z:\mingw32bit.483
 set CMAKE=z:\sw\cmake
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%GCCDIR%\bin;%CMAKE%\bin
-z:\msys64\msys2_shell.bat
+z:\msys2\msys2_shell.bat
